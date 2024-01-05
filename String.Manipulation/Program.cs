6fa3c0301d29ecc7a574682640dd2dd988f91233
SejiMe <atienza.seji@gmail.com>
2024-01-05 14:48:43 +0800

    Add project files.

---
+++ String.Manipulation/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string word = "+6398812923";
+
+var subsrting = word.Substring(3);
+
+Console.WriteLine(subsrting);
+
+string name = "Name, Firstname";
+
+var spllittedName = name.Split(",");
+
+foreach (string s in spllittedName)
+{
+    Console.WriteLine(s);
+}
